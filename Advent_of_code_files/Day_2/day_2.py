bd441e1256097a0ea0cc77ae5d8df355a9064fe0
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-12-11 17:58:43 +0500

    Update day_2.py

--- Advent_of_code_files/Day_2/day_2.py
+++ Advent_of_code_files/Day_2/day_2.py
@@ -1,22 +1,30 @@
 # Advent of code problem number 2 starts here #
 with open('C:\Personal Files\OneDrive - Habib University\Python\Extra\elfs.txt', 'r') as f:
     fh = f.readlines()
-    game_sum = 0
+    game_sum, line_number = 0, 0
     for line in fh: # Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
         flag = True
-        game_number = line[5]
-        lst = line[8:].split(";") # >= 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        if line_number < 10:
+            game_number = line[5]
+            lst = line[8:].split(";") # >= 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        elif line_number < 100:
+            game_number = line[5:7]
+            lst = line[9:].split(";")
+        elif line_number >= 100:
+            game_number = line[5:8]
+            lst = line[10:].split(";")
         for items in lst: # 1 blue, 2 green
             item_list = items.strip().split(',')
             for colour in item_list: # 1 blue  # 2 green
                 if 'blue' in colour:
-                    if int(colour.strip()[:2]) >= 14: flag = False
-                if 'red' in colour:
-                    if int(colour.strip()[:2]) >= 12: flag = False
-                if 'green' in colour:
-                    if int(colour.strip()[:2]) >= 13: flag = False
+                    if int(colour.strip()[:2]) > 14: flag = False
+                elif 'red' in colour:
+                    if int(colour.strip()[:2]) > 12: flag = False
+                elif 'green' in colour:
+                    if int(colour.strip()[:2]) > 13: flag = False
         if flag is False: flag = True
         else: game_sum += int(game_number)
+        line_number += 1
 print(game_sum)
 f.close()
 # Advent of code problem number 2 ends here #
